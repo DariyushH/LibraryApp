431d321444b3d4c2b34ad4b1e5fef20639514a86
DariyushH <haidutski.d@gmail.com>
2024-09-17 23:14:46 +0300

    Add localization.

--- src/main/java/org/example/books/config/BookAppConfig.java
+++ src/main/java/org/example/books/config/BookAppConfig.java
@@ -1,16 +1,23 @@
 package org.example.books.config;
 
-import org.example.books.repository.BookRepository;
-import org.example.books.service.BookService;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.books")
 
 public class BookAppConfig {
-
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    
 
     }
 
