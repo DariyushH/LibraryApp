2219e8c4cfa99be301b3614446959bf132a4d273
DariyushH <haidutski.d@gmail.com>
2024-09-21 13:13:31 +0300

    Adding logging and caching.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,8 @@ dependencies {
     implementation ("org.slf4j:slf4j-simple:2.0.7")
     testImplementation ("junit:junit:4.13.2")
     implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.0")
-
+    implementation("org.springframework:spring-aop:5.2.9.RELEASE")
+    implementation("org.springframework:spring-aspects:5.2.9.RELEASE")
 }
 
 tasks.test {
